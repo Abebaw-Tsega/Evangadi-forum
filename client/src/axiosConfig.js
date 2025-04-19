33c0042f33b077a68bc4f90222436df6ce13dea2
Abebaw <abebawtsega3@gmail.com>
2025-04-19 09:21:11 +0300

    someChange

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosBase = axios.create({
-  baseURL: 'http://localhost:5555/api',
+  // baseURL: 'http://localhost:5555/api',
+  baseURL: 'https://evangadi-forum-backend-deploy-2-hc59.onrender.com/',
 });
 
 export default axiosBase;
